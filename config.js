c2a1b84bf160f97b77b0d4679bca3baff2fde3bb
John Chase <ratteal@gmail.com>
2019-09-27 10:29:26 +0100

    Initial Twitter API configuration and creation.

---
+++ config.js
@@ -0,0 +1,14 @@
+// Load the Twitter API keys from environment variables, the command line or a file.
+
+const nconf = require('nconf');
+
+nconf.env()
+    .argv()
+    .file({ file: './config.json' });
+
+module.exports = {
+    consumer_key: nconf.get("TWITTER_CONSUMER_KEY"),
+    consumer_secret: nconf.get("TWITTER_CONSUMER_SECRET"),
+    access_token: nconf.get("TWITTER_ACCESS_TOKEN"),
+    access_token_secret: nconf.get("TWITTER_ACCESS_TOKEN_SECRET")
+};
---
+++ index.js
@@ -0,0 +1,8 @@
+// Use the Twit node package.
+const Twit = require('twit');
+
+// Pull in the Twitter account keys.
+const config = require('./config.js');
+
+// Instantiate the API object.
+const T = new Twit(config);

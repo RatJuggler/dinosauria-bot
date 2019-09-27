beec4830a4a4909f6725eff98eeaa18951754466
John Chase <ratteal@gmail.com>
2019-09-27 14:51:34 +0100

    Basic URL checking code.

--- app/index.js
+++ app/index.js
@@ -14,3 +14,17 @@ const dinosaur = require('./dinosaur.js');
 winston.info("Booting dinosauria bot...");
 
 winston.info("Chosen dinosaur: " + dinosaur.name);
+
+const https = require('https');
+const url = require('url');
+
+let options = {
+    method: 'HEAD',
+    host: url.parse(dinosaur.wikiURL).host,
+    port: url.parse(dinosaur.wikiURL).port,
+    path: url.parse(dinosaur.wikiURL).path
+};
+let req = https.request(options, function(r) {
+    winston.debug(r.statusCode + " = " + r.statusMessage);
+    winston.info("Chosen dinosaur: " + dinosaur.name);
+}).end();

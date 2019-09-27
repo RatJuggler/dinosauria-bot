5ac20c4885fadd2f477503e8b68f432898555bfc
John Chase <ratteal@gmail.com>
2019-09-27 16:16:36 +0100

    Split out the log line format.

--- app/winston.js
+++ app/winston.js
@@ -1,12 +1,14 @@
 const winston = require("winston");
 
+const logFormat = winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+);
+
 // Instantiate and export a logger.
 const logger = new winston.createLogger({
     level: "debug",
-    format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json()
-    ),
+    format: logFormat,
     transports: [
         new winston.transports.Console({
             handleExceptions: true

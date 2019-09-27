811f576ac3806411d39709e11db63ed2d95456b5
John Chase <ratteal@gmail.com>
2019-09-27 15:19:23 +0100

    Move URL validation to form part of dinosaur selection.

--- app/dinosaur.js
+++ app/dinosaur.js
@@ -1,15 +1,40 @@
 // Pull in the list of dinosaurs.
+const winston = require('./winston.js');
 const dinosaurs = require('dinosaurs');
+const https = require('https');
+const url = require('url');
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-function getDinosaur() {
+async function validURL(urlToValidate) {
+    winston.debug("Validating URL: " + urlToValidate);
+    let options = {
+        method: 'HEAD',
+        host: url.parse(urlToValidate).host,
+        port: url.parse(urlToValidate).port,
+        path: url.parse(urlToValidate).path
+    };
+    let isValid = await new Promise(resolve => {
+        https.request(options, result => resolve(result.statusCode === 200)).end();
+    })
+}
+
+function getRandomName() {
     let rndDino = getRandomInt(dinosaurs.length);
     let dinoName = dinosaurs[rndDino];
+    winston.debug("Selected random name: " + dinoName);
     dinoName = dinoName.charAt(0).toUpperCase() + dinoName.slice(1);
-    let wikiURL = "https://en.wikipedia.org/wiki/" + dinoName;
+    return dinoName;
+}
+
+function getDinosaur() {
+    let dinoName, wikiURL;
+    do {
+        dinoName = getRandomName();
+        wikiURL = "https://en.wikipedia.org/wiki/" + dinoName;
+    } while (!validURL(wikiURL));
     return {"name": dinoName, "wikiURL": wikiURL}
 }
 
--- app/index.js
+++ app/index.js
@@ -8,23 +8,9 @@ const config = require('./config.js');
 // Instantiate the API object.
 const T = new Twit(config);
 
+winston.info("Booting dinosauria bot...");
+
 // Pull in the dinosaur lookup.
 const dinosaur = require('./dinosaur.js');
 
-winston.info("Booting dinosauria bot...");
-
 winston.info("Chosen dinosaur: " + dinosaur.name);
-
-const https = require('https');
-const url = require('url');
-
-let options = {
-    method: 'HEAD',
-    host: url.parse(dinosaur.wikiURL).host,
-    port: url.parse(dinosaur.wikiURL).port,
-    path: url.parse(dinosaur.wikiURL).path
-};
-let req = https.request(options, function(r) {
-    winston.debug(r.statusCode + " = " + r.statusMessage);
-    winston.info("Chosen dinosaur: " + dinosaur.name);
-}).end();

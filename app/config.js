6a1f6a260db1f32935288c4c09fb437d0267f272
John Chase <ratteal@gmail.com>
2019-10-01 16:10:13 +0100

    Simplify API creation and tweeting.

--- app/config.js
+++ app/config.js
@@ -1,14 +1,17 @@
-// Load the Twitter API keys from environment variables, the command line or a file.
+// Load the Twitter API keys from environment variables, the command line or a file and initialise the API interface.
 
-const nconf = require('nconf');
+const nConf = require('nconf');
+const Twit = require('twit');
 
-nconf.env()
+nConf.env()
     .argv()
     .file({ file: 'app/config.json' });
 
-module.exports = {
-    consumer_key: nconf.get("TWITTER_CONSUMER_KEY"),
-    consumer_secret: nconf.get("TWITTER_CONSUMER_SECRET"),
-    access_token: nconf.get("TWITTER_ACCESS_TOKEN"),
-    access_token_secret: nconf.get("TWITTER_ACCESS_TOKEN_SECRET")
+const twitterKeys = {
+    consumer_key: nConf.get("TWITTER_CONSUMER_KEY"),
+    consumer_secret: nConf.get("TWITTER_CONSUMER_SECRET"),
+    access_token: nConf.get("TWITTER_ACCESS_TOKEN"),
+    access_token_secret: nConf.get("TWITTER_ACCESS_TOKEN_SECRET")
 };
+
+module.exports = new Twit(twitterKeys);
--- app/tweet.js
+++ app/tweet.js
@@ -1,24 +1,14 @@
 const winston = require('./winston.js');
-const Twit = require('twit');
+const twitterAPI = require('./config.js');
 
-// Pull in the Twitter account keys.
-const config = require('./config.js');
-
-function tweetTest() {
-    // Instantiate the API object.
-    const T = new Twit(config);
-
-    T.post('statuses/update', { status: 'Hello Pangaea!' }, function(err, data, response) {
+function tweet(update) {
+    twitterAPI.post('statuses/update', { status: update }, function(err, data, response) {
         if (err) {
             winston.error("Problem Tweeting: " + err);
         } else {
-            winston.debug("Tweet Data: " + data.text);
+            winston.info("Tweet Data:\n" + data.toString());
         }
     });
-}
-
-function tweet(dinosaur) {
-    winston.debug("Tweet: " + JSON.stringify(dinosaur));
 }
 
 module.exports.tweet = tweet;

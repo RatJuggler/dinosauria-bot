98cabbb8c57aee3a4f4e06f8c5ee496e597b889d
John Chase <ratteal@gmail.com>
2019-10-02 11:24:33 +0100

    Test Twitter credentials.

--- app/tweet.js
+++ app/tweet.js
@@ -1,6 +1,23 @@
 const winston = require('./winston.js');
 const twitterAPI = require('./config.js');
 
+function verifyCredentials() {
+    let parameters = {
+        include_entities: false,
+        skip_status: true,
+        include_email: true
+    };
+    twitterAPI.get('account/verify_credentials', parameters, (err, data) => {
+        if (err) {
+            winston.error(err.code + ' : ' + err.message);
+            throw new Error("Unable to verify Twitter credentials!");
+        } else {
+            winston.debug(JSON.stringify(data));
+            winston.info("Twitter credentials verified.");
+        }
+    })
+}
+
 function tweet(update) {
     twitterAPI.post('statuses/update', { status: update }, function(err, data, response) {
         if (err) {
@@ -10,5 +27,7 @@ function tweet(update) {
         }
     });
 }
+
+verifyCredentials();
 
 module.exports.tweet = tweet;

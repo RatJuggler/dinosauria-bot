4cddfcbdc48f004fce38ccd90d0281f3b27280c3
John Chase <ratteal@gmail.com>
2019-10-04 09:40:03 +0100

    Switch to parsing Wikimedia markup.

--- app/wikipedia.js
+++ app/wikipedia.js
@@ -1,41 +1,22 @@
 const winston = require('./winston.js');
-const cheerio = require('cheerio');
 
-function stripUnwanted(html) {
-    html = html.replace(/(<([^>]+)>)/gi, '');
-    winston.debug("Removed HTML Elements: " + html);
-    html = html.replace(/\/.*?\//gi, '');
-    winston.debug("Removed Unicode Pronunciation: " + html);
-    html = html.replace(/&apos;|&quot;/gi, '');
-    winston.debug("Removed HTML Escapes: " + html);
-    html = html.replace(/&#[x0-9A-Z]*;/, '');
-    winston.debug("Removed Remaining Unicode: " + html);
-    html = html.replace(/Chinese: .*?; /gi, '');
-    winston.debug("Removed Chinese: " + html);
-    html = html.replace(/Pinyin: .*?; /gi, '');
-    winston.debug("Removed Pinyin: " + html);
-    return html;
+function stripUnwanted(markup) {
+    let text = markup.replace(/'''''/gi, '');
+    winston.debug("Removed bold/italic: " + text);
+    text = markup.replace(/\[\[|\]\]/gi, '');
+    winston.debug("Removed all square brackets: " + text);
+    return text;
 }
 
 function findSomeText(body) {
-    let $ = cheerio.load(body);
-    let firstParagraph = $("p i b").closest('p').html();
-    winston.debug("firstParagraph: " + firstParagraph);
-    let rawText = '';
-    if (firstParagraph) {
-        let firstParentheses = firstParagraph.match(/<\/b><\/i> \(.*?\)/);
-        winston.debug("firstParentheses: " + firstParentheses);
-        if (firstParentheses) {
-            rawText = stripUnwanted(firstParentheses.toString());
-        } else {
-            let isAText = firstParagraph.match(/<\/b><\/i> is a .*?([,.])/);
-            if (isAText) {
-                rawText = stripUnwanted((isAText.toString()));
-            }
-        }
-    }
-    winston.debug("rawText: " + rawText);
-    return rawText;
+    let markup = JSON.parse(body);
+    let wikitext = markup.parse.wikitext;
+    winston.debug("From Wiki: "+ JSON.stringify(markup.parse.wikitext));
+    // TODO: Deal with redirects correctly.
+    if (wikitext.contains('#redirect')) {return}
+    let initialText = wikitext.match(/'''''.*?[.,]/);
+    winston.debug("initialText: " + initialText);
+
 }
 
 module.exports.findSomeText = findSomeText;

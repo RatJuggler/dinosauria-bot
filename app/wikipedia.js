a29897e9512003ed7428e8942ab1ce2335c540dd
John Chase <ratteal@gmail.com>
2019-10-04 12:37:57 +0100

    More parsing tweaks and limit length of text returned.

--- app/wikipedia.js
+++ app/wikipedia.js
@@ -12,7 +12,7 @@ function removeBrace(markup, startingFrom, openBrace, closeBrace) {
 }
 
 function removeBraceSections(markup, openBrace, closeBrace) {
-    let braceStart = -1;
+    let braceStart;
     while ((braceStart = markup.indexOf(openBrace)) !== -1) {
         markup = removeBrace(markup, braceStart, openBrace, closeBrace);
     }
@@ -20,17 +20,23 @@ function removeBraceSections(markup, openBrace, closeBrace) {
 }
 
 function stripUnwanted(markup) {
-    markup = markup.replace(/'''''/gi, '');
+    markup = markup.replace(/'''/gi, '');
+    markup = markup.replace(/''/gi, '');
     winston.debug("Removed bold/italic:\n" + markup);
-    markup = markup.replace(/\[\[|\]\]/gi, '');
+    markup = markup.replace(/\|.*?]/gi, ']');
+    winston.debug("Removed page names:\n" + markup);
+    markup = markup.replace(/\[\[|]]/gi, '');
     winston.debug("Removed all square brackets:\n" + markup);
+    markup = markup.replace(/&nbsp;/gi, ' ');
+    winston.debug("Removed &nbsp;:\n" + markup);
     return markup;
 }
 
 function findSomeText(body, textSize) {
     let markup = JSON.parse(body);
     let wikiText = markup.parse.wikitext['*'];
-    wikiText = wikiText.replace(/\n/gi, '');
+    // Replace all newlines with spaces to help with parsing.
+    wikiText = wikiText.replace(/\n/gi, ' ');
     winston.debug("From Wiki:\n"+ wikiText);
     // TODO: Deal with redirects correctly.
     if (wikiText.includes('#redirect') || wikiText.includes('#REDIRECT')) {return;}
@@ -38,9 +44,17 @@ function findSomeText(body, textSize) {
     winston.debug("Removed {} sections:\n" + wikiText);
     wikiText = removeBraceSections(wikiText, '<', '>');
     winston.debug("Removed <> sections:\n" + wikiText);
+    if (wikiText.startsWith('[')) {
+        wikiText = removeBrace(wikiText, 0, '[', ']');
+        winston.debug("Removed initial [] section:\n" + wikiText);
+    }
     wikiText = stripUnwanted(wikiText);
-    wikiText = wikiText.match(/.*?\./);
+    wikiText = wikiText.match(/.*?\. /);
+    wikiText = String(wikiText).trim();
     winston.debug("First Sentence:\n" + wikiText);
+    if (wikiText.length > textSize) {
+        wikiText = wikiText.slice(0, textSize-3) + '...';
+    }
     return wikiText;
 }
 
